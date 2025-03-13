f6935c41ddd1942e893312db0c9ae4166f0bf6bf
pedestrianlove <jsli@linux.com>
2025-03-13 17:37:26 +0800

    feat: finish lambda activity

--- 01-cloud-foundations/lambda-activity/lambda_function.tf
+++ 01-cloud-foundations/lambda-activity/lambda_function.tf
@@ -3,15 +3,15 @@ data "archive_file" "lambda_zip" {
   output_path = "lambda_function_payload.zip"
   source {
     content  = <<EOF
-      import boto3
-      region = 'us-east-1'
-      instances = ['${data.aws_instance.lab_instance.id}']
-      ec2 = boto3.client('ec2', region_name=region)
+import boto3
+region = 'us-east-1'
+instances = ['${data.aws_instance.lab_instance.id}']
+ec2 = boto3.client('ec2', region_name=region)
 
-      def lambda_handler(event, context):
-          ec2.stop_instances(InstanceIds=instances)
-          print('stopped your instances: ' + str(instances))
-      EOF
+def lambda_handler(event, context):
+    ec2.stop_instances(InstanceIds=instances)
+    print('stopped your instances: ' + str(instances))
+    EOF
     filename = "lambda_function.py"
   }
 }
@@ -26,7 +26,7 @@ resource "aws_lambda_function" "lab_stopinator" {
   function_name    = "myStopinator"
   role             = data.aws_iam_role.iam_for_lambda.arn
   filename         = "lambda_function_payload.zip"
-  handler          = "lambda_function.py"
+  handler          = "lambda_function.lambda_handler"
   runtime          = "python3.11"
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 }
